247f3c0bdd44cca293ade8f345135ea4caea66a9
cookednick <cookednick@users.noreply.github.com>
2023-05-22 03:19:03 -0700

    Updated modules & Removed copy of Objective-C

--- modules/Objective_C/LightweightRenderingView/build.sh
+++
@@ -1,5 +0,0 @@
-gcc -o libLightweightRenderingView.dylib -shared LightweightRenderingView.m -framework AppKit -framework QuartzCore
-install_name_tool -id "@rpath/libLightweightRenderingView.dylib" libLightweightRenderingView.dylib
-
-gcc -c LightweightRenderingView.m
-libtool -static -c -o libLightweightRenderingView.a LightweightRenderingView.o
