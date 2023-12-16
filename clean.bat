20d48aaae32e69c955c5fd585c107a4416cf5c17
Ivan Ivanov <ivan+focus@ivanovs.info>
2023-12-17 10:27:33 +1300

    Remove batch scripts. Re #238

--- clean.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-
-del *.exe
-del *.pdb
-
-rmdir /s build_debug
-rmdir /s build_release
--- run.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-
-jai -quiet first.jai
-
-if %ERRORLEVEL% EQU 0 (
-    pushd build_debug
-    focus.exe
-    popd
-)
